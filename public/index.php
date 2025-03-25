b69ba9ef9eb687f0c6f7388e1ea1120728623da9
Oleguer Mas <oleguer.masilarraneta@zitcha.com>
2025-03-25 15:28:27 +0100

    composer require tempest/framework:1.0-alpha.6 && ./vendor/bin/tempest install framework

--- public/index.php
+++ public/index.php
@@ -2,4 +2,10 @@
 
 declare(strict_types=1);
 
-echo "Hello, world!\n";
+use Tempest\Router\HttpApplication;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+HttpApplication::boot(__DIR__ . '/../')->run();
+
+exit();
